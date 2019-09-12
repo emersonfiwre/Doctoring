da9edc2bb54940029cc7b0e256ecbf34308fdea3
Server-Jin.i5 <lima.emerson2009@gmail.com>
2019-09-12 14:57:31 -0300

    adapter finallized

--- app/src/main/java/com/intacta/doctoring/MainActivity.java
+++ app/src/main/java/com/intacta/doctoring/MainActivity.java
@@ -5,8 +5,14 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
     RecyclerView rvDatas;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         rvDatas = findViewById(R.id.rv_datas);
+
+        startFirebase();
+
+    }
+    private void startFirebase(){
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
 
     }
 }
